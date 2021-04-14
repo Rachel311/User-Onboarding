f39da6a7a658c8dfed3206aaef5155b73e69d8da
Tory Rachel Van Tassel <countrygirlr09@hotmail.com>
2021-04-13 21:22:00 -0600

    Completed App, Form, and Schema

---
+++ myuseronboarding/src/App.js
@@ -0,0 +1,92 @@
+// import logo from './logo.svg';
+import './App.css';
+import React, { useState, useEffect } from 'react';
+import * as yup from 'yup';
+import { Route } from 'react-router-dom';
+import axios from 'axios';
+
+import schema from './Components/Schema';
+import Form from './Components/Form';
+import Users from './Components/Users';
+
+const initialValues = {
+  first_name:'',
+  last_name:'',
+  email:'',
+  password:'',
+  title:'',
+  terms: false
+}
+
+const initialErrors = {
+  first_name:'',
+  last_name:'',
+  email:'',
+  password:'',
+  title:'',
+}
+
+
+function App() {
+
+  const [user, setUser] = useState([])
+  const [formValues, setFormValues] = useState(initialValues)
+  const [formErrors, setFormErrors] = useState(initialErrors)
+  const [disabled, setDisabled] = useState(true)
+
+
+  const newUserForm = (event) =>{
+
+    const newUser = {
+      first_name:formValues.first_name.trim(),
+      last_name:formValues.last_name.trim(),
+      email:formValues.email.trim(),
+      title:formValues.title.trim(),
+      terms:formValues.terms
+    }
+    postNewUser(newUser)
+  }
+
+  const postNewUser = (newUser) =>{
+    axios.post(`https://reqres.in/api/users`, newUser)
+    .then((res)=>{
+      setUser([res.data, ...newUser])
+      setFormValues(initialValues)
+    })
+    .catch((Err) => {
+      console.log(Error)
+    })
+  }
+
+  const onChange = (name, value) => {
+    yup
+    .reach(schema, name)
+    .validate(value)
+    .then(() => {
+      setFormErrors({...formErrors,[name]: '',})
+    })
+    .catch((err) => {
+      setFormErrors({...formErrors,[name]: err.errors[0],
+      })
+    })
+    setFormValues({...formValues, [name]: value,})
+  }
+
+  useEffect(() => {
+    schema.isValid(formValues).then((valid) => {
+      setDisabled(!valid);
+    });
+  }, [formValues]);
+
+
+
+  return (
+    <div className="App">
+      <Route path='/Form'>
+        <Form onChange={onChange} values={formValues} newUserForm={newUserForm} disabled={disabled} errors={formErrors}/></Route>
+      <Route path='/Users'><Users/></Route>
+    </div>
+  );
+}
+
+export default App;
---
+++ myuseronboarding/src/Components/Form.js
@@ -0,0 +1,110 @@
+import React from 'react';
+import styled from 'styled-components';
+
+
+export default function Form({values, onChange, newUserForm, disabled, errors}){
+
+    const onSubmit = (event) => {
+        event.preventDefault()
+        newUserForm()
+    }
+
+    const onChanger = (e) => {
+        const { name, value, type, checked } = e.target
+        const values = type === "checkbox" ? checked : value
+        onChange(name, values)
+    }
+
+    const InputBox = styled.input`
+    width: 95%;
+    height: 56px;
+    border-radius: 4px;
+    position: relative;
+    background-color: rgba(255,255,255,0.3);
+    transition: 0.3s all;
+    text-align:center;
+    &:hover {
+    background-color: rgba(255, 255, 255, 0.45);
+    box-shadow: 0px 4px 20px 0px rgba(0, 0, 0, 0.05);
+    }`
+
+
+
+    return(
+        <div>
+            <form onSubmit={onSubmit}>
+
+                <div>
+                    <div>{errors.first_name}</div>
+                    <label>First name:
+                    <InputBox
+                    name='first_name'
+                    type='text'
+                    placeholder='first name'
+                    onChange={onChanger}
+                    value={values.first_name}/>
+                    </label>
+                </div>
+                <div>
+                    <div>{errors.last_name}</div>
+                    <label>Last Name:
+                    <InputBox
+                    name= 'last_name'
+                    type='text'
+                    placeholder='last name'
+                    onChange={onChanger}
+                    value={values.last_name}
+                    />
+                </label>
+                </div>
+                <div>
+                    <div>{errors.email}</div>
+                    <label><div>Email:</div>
+                    <InputBox
+                    name='email'
+                    type='email'
+                    placeholder='email'
+                    onChange={onChanger}
+                    value={values.email}
+                    />
+                    </label>
+                </div>
+                <div>
+                <div>{errors.password}</div>
+                <label><div>Password:</div>
+                    <InputBox
+                    name='password'
+                    type='password'
+                    placeholder='password'
+                    onChange={onChanger}
+                    value={values.password}
+                    />
+                </label>
+                </div>
+                <div>
+                <label>Title:
+                    <div>
+                  <select name='title' onChange = {onChanger} value= {values.title}>
+                      <option value=''>--Select Role--</option>
+                      <option value='Student'>Student</option>
+                      <option value='Instructor'>Instructor</option>
+                  </select>
+                  </div>
+                  </label>
+                </div>
+
+                <div>
+                    <div>{errors.terms}</div>
+                    <label><div>Terms and conditions:</div>
+                    <input
+                    type='checkbox'
+                    />
+                    </label>
+                </div>
+
+                <button disabled={disabled}>Submit</button>
+
+            </form>
+        </div>
+    )
+}
